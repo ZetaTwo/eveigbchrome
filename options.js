442220fc82e7f56bb7142a0d7df2412130a240d4
Calle Svensson <calle.svensson@zeta-two.com>
2014-03-15 14:01:40 +0100

    Added Chrome Bootstrap

--- options.js
+++ options.js
@@ -5,6 +5,8 @@ function init() {
   host_field = document.getElementById("form_host");
   char_field = document.getElementById("form_char");
 
+  document.querySelector('form#options_form').addEventListener('submit', save_data);
+
   document.getElementById("loc_title").innerHTML = chrome.i18n.getMessage("options_title");
   document.getElementById("loc_host").innerHTML = chrome.i18n.getMessage("options_host") + ":";
   document.getElementById("loc_char").innerHTML = chrome.i18n.getMessage("options_char");
@@ -36,4 +38,30 @@ function save_data(evt) {
 }
 
 document.addEventListener('DOMContentLoaded', init);
-document.querySelector('form#options_form').addEventListener('submit', save_data);
+
+//Styling and menu
+$(function() {
+  $('.menu a').click(function(ev) {
+    ev.preventDefault();
+    var selected = 'selected';
+
+    $('.mainview > *').removeClass(selected);
+    $('.menu li').removeClass(selected);
+    setTimeout(function() {
+      $('.mainview > *:not(.selected)').css('display', 'none');
+    }, 100);
+
+    $(ev.currentTarget).parent().addClass(selected);
+    var currentView = $($(ev.currentTarget).attr('href'));
+    currentView.css('display', 'block');
+    setTimeout(function() {
+      currentView.addClass(selected);
+    }, 0);
+
+    setTimeout(function() {
+      $('body')[0].scrollTop = 0;
+    }, 200);
+  });
+
+  $('.mainview > *:not(.selected)').css('display', 'none');
+});
